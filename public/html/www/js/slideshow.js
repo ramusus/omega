7383727fe42d4bcbea896730b7ecbe1eba587bb8
ram <ramusus@gmail.com>
2012-02-13 20:27:46 +0600

    updated gallery

---
+++ public/html/www/js/slideshow.js
@@ -0,0 +1,108 @@
+var slideShow = (function(){
+	var SETTINGS = {
+		container: '#gallery',
+		slidesContainerSelector: '.slides', 
+		slideSelector: '.slide',
+		prevLinkSelector: '.b-control-prev',
+		nextLinkSelector: '.b-control-next',
+		prevLinkDisabledClass: 'b-control-disabled-prev',
+		nextLinkDisabledClass: 'b-control-disabled-next',
+		dotsContainerSelector: '.dots',
+		dotSelector: '.b-dot',
+		dotSample: '<a class="b-dot" href="?"><b></b></a>',
+		dotSelectedClass: 'b-dot-selected',
+		slideWidth: 612
+	};
+	
+	var _container, _prevLink, _nextLink,
+		_dotsContainer, _dots,
+		_slidesCount, _currentSlide;
+	
+	function prepareDots(){
+		var dotsHtml = '';
+		
+		for( var i = 0; i < _slidesCount; i++ ){
+			dotsHtml += SETTINGS.dotSample;
+		}
+		
+		_dotsContainer.html(dotsHtml);
+		_dots = $(SETTINGS.dotSelector, _dotsContainer);
+	}
+	
+	function manageLinks(){
+		if( _currentSlide == 0 ){
+			_prevLink.addClass(SETTINGS.prevLinkDisabledClass);
+		}
+		else if( _prevLink.hasClass(SETTINGS.prevLinkDisabledClass) ){
+			_prevLink.removeClass(SETTINGS.prevLinkDisabledClass);
+		}
+		
+		if( _currentSlide == _slidesCount - 1 ){
+			_nextLink.addClass(SETTINGS.nextLinkDisabledClass);
+		}
+		else if( _nextLink.hasClass(SETTINGS.nextLinkDisabledClass) ){
+			_nextLink.removeClass(SETTINGS.nextLinkDisabledClass);
+		}
+		
+		_dots.removeClass(SETTINGS.dotSelectedClass);
+		_dots.eq(_currentSlide).addClass(SETTINGS.dotSelectedClass);
+	}
+	
+	function assignEvents(){
+		_prevLink.click(function(event){
+			if( _currentSlide > 0 ){
+				_currentSlide--;
+				switchSlide();
+			}
+			
+			event.preventDefault();
+		});
+		
+		_nextLink.click(function(event){
+			if( _currentSlide < _slidesCount - 1 ){
+				_currentSlide++;
+				switchSlide();
+			}
+			
+			event.preventDefault();
+		});
+		
+		_dots.click(function(event){
+			var thisIndex = $(this).index();
+			
+			if( thisIndex != _currentSlide ){
+				_currentSlide = thisIndex;
+				switchSlide();
+			}
+			
+			event.preventDefault();
+		});
+	}
+	
+	function switchSlide(){
+		_slidesContainer.animate({
+			marginLeft: _currentSlide * SETTINGS.slideWidth * -1
+		}, 300);
+		
+		manageLinks();
+	}
+	
+	return {
+		init: function(userSettings){
+			$.extend(SETTINGS, userSettings);
+			
+			_container = $(SETTINGS.container);
+			_slidesContainer = $(SETTINGS.slidesContainerSelector, _container);
+			_prevLink = $(SETTINGS.prevLinkSelector, _container);
+			_nextLink = $(SETTINGS.nextLinkSelector, _container);
+			_dotsContainer = $(SETTINGS.dotsContainerSelector, _container);
+			
+			_currentSlide = 0;
+			_slidesCount = $(SETTINGS.slideSelector, _slidesContainer).length;
+			
+			prepareDots();
+			manageLinks();
+			assignEvents();
+		}
+	};
+})();
