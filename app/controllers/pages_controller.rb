9984ff0ea940c004eab0684c54234ad7c6cec167
ram <ramusus@gmail.com>
2012-02-24 19:28:55 +0600

    added english version to templates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def show
     @page = Page.find(:first, :conditions => ["slug = ?", params[:slug] ? params[:slug] : ''])
     @presses = Press.limit(4)
+    @en = (@page.slug == 'english')
 
     respond_to do |format|
       format.html # show.html.erb
